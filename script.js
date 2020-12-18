d71ecf89b93fc60509fc002831d05d5c1c066d0a
Eduardo <eduuhliima97@gmail.com>
2020-12-18 16:07:08 -0300

    quase concluido

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function SlideShow() {
 }
 
 //Change word function
-let words = ["Magnific", "Best", "Easy"];
+let words = ["Easy to recreate", "zoom effect with CSS", "A cool template"];
 let wordsIndividual = 0;
 setInterval(() => {
     document.getElementById("letter").innerHTML = words[wordsIndividual++]
@@ -37,10 +37,7 @@ function typeWriter() {
     if (i < txt.length) {
         document.getElementById("outputWritter").innerHTML += txt.charAt(i);
         i++;
-    }/*else if(i === txt.length){
-       txt.length
-        i = 0;
-    }*/
+    }
     setTimeout(typeWriter, 100);
 }
 window.onload = typeWriter;
@@ -50,13 +47,13 @@ window.onload = typeWriter;
 window.onscroll = () => {
     scrollF();
 }
-function scrollF(){
-    if(document.body.scrollTop > 500 || document.documentElement.scrollTop >500) {
-        document.getElementById("arrowHome").style.color = "red";
-    }else {
-        document.getElementById("arrowHome").style.color = "yellow";
+function scrollF() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        document.getElementById("arrowHome").style.display = "none";
+    } else {
+        document.getElementById("arrowHome").style.display = "none";
     }
-    
+
 }
 
 
@@ -69,7 +66,7 @@ function responsiveBtn() {
     } else {
         menu.style.visibility = "visible";
     }
-    
+
 }
 
 //Hide nav_responsive when sroll to down
@@ -94,10 +91,162 @@ window.onscroll = function () {
 */
 //Video Just Javascript
 let video = document.getElementById("videoShow");
-video.addEventListener("click", ()=>  {
+video.addEventListener("click", () => {
     document.getElementById("videoframe").style.display = "block";
-    document.getElementById("videoYoutube").src += "?autoplay=1";
+    document.getElementById("videoYoutube").src += "?autoplay=1&mute=1";
 })
-    
+
+//Modals box code
+const modals = [`modal1, modal2, modal3, modal4, modal5, modal6`];
+const btns = [`btn1, btn2, btn3, btn4, btn5, btn6`]
+const closes = [`close1, close2, close3, close4, close5`]
+
+closes[0] = document.getElementsByClassName("close")[0];
+modals[0] = document.getElementById("modal_1");
+btns[0] = document.getElementById("btnModalCode1");
+
+closes[1] = document.getElementsByClassName("close_2")[0];
+modals[1] = document.getElementById("modal_2");
+btns[1] = document.getElementById("btnModalCode2");
+
+closes[2] = document.getElementsByClassName("close_3")[0];
+modals[2] = document.getElementById("modal_3");
+btns[2] = document.getElementById("btnModalCode3");
+
+closes[3] = document.getElementsByClassName("close_4")[0];
+modals[3] = document.getElementById("modal_4");
+btns[3] = document.getElementById("btnModalCode4");
+
+closes[4] = document.getElementsByClassName("close_5")[0];
+modals[4] = document.getElementById("modal_5");
+btns[4] = document.getElementById("btnModalCode5");
+
+closes[5] = document.getElementsByClassName("close_6")[0];
+modals[5] = document.getElementById("modal_6");
+btns[5] = document.getElementById("btnModalCode6");
+
+closes[6] = document.getElementsByClassName("close_7")[0];
+modals[6] = document.getElementById("modal_7");
+btns[6] = document.getElementById("btnModalCode7");
+
+closes[7] = document.getElementsByClassName("close_8")[0];
+modals[7] = document.getElementById("modal_8");
+btns[7] = document.getElementById("btnModalCode8");
+
+closes[8] = document.getElementsByClassName("close_9")[0];
+modals[8] = document.getElementById("modal_9");
+btns[8] = document.getElementById("btnModalCode9");
 
 
+btns[0].onclick = () => {
+    modals[0].style.display = "flex";
+}
+closes[0].onclick = () => {
+    modals[0].style.display = "none";
+}
+
+btns[1].onclick = () => {
+    modals[1].style.display = "flex";
+}
+closes[1].onclick = () => {
+    modals[1].style.display = "none";
+}
+
+btns[2].onclick = () => {
+    modals[2].style.display = "flex";
+}
+closes[2].onclick = () => {
+    modals[2].style.display = "none";
+}
+
+btns[3].onclick = () => {
+    modals[3].style.display = "flex";
+}
+closes[3].onclick = () => {
+    modals[3].style.display = "none";
+}
+
+btns[4].onclick = () => {
+    modals[4].style.display = "flex";
+}
+closes[4].onclick = () => {
+    modals[4].style.display = "none";
+}
+
+btns[5].onclick = () => {
+    modals[5].style.display = "flex";
+}
+closes[5].onclick = () => {
+    modals[5].style.display = "none";
+}
+
+btns[6].onclick = () => {
+    modals[6].style.display = "flex";
+}
+closes[6].onclick = () => {
+    modals[6].style.display = "none";
+}
+
+btns[7].onclick = () => {
+    modals[7].style.display = "flex";
+}
+closes[7].onclick = () => {
+    modals[7].style.display = "none";
+}
+
+btns[8].onclick = () => {
+    modals[8].style.display = "flex";
+}
+closes[8].onclick = () => {
+    modals[8].style.display = "none";
+}
+
+//Click outside of box modal too close the modal
+window.onclick = function (event) {
+    if (event.target == modals[0] || event.target == modals[1] || event.target == modals[2]
+        || event.target == modals[3] || event.target == modals[4] || event.target == modals[5]
+        || event.target == modals[6] || event.target == modals[7] || event.target == modals[8]) {
+        modals[0].style.display = "none";
+        modals[1].style.display = "none";
+        modals[2].style.display = "none";
+        modals[3].style.display = "none";
+        modals[4].style.display = "none";
+        modals[5].style.display = "none";
+        modals[6].style.display = "none";
+        modals[7].style.display = "none";
+        modals[8].style.display = "none";
+
+    }
+}
+
+
+//Contact
+const user = document.getElementById("user");
+const email = document.getElementById("email");
+const textArea = document.getElementById("textareaJ");
+const form = document.getElementById("form");
+const errorElement = document.getElementById('error');
+
+form.addEventListener('submit', (e) => {
+    let messages = []
+    if (user.value === '' || user.value == null) {
+        messages.push('Name is required!');
+        errorElement.style.display = "flex";
+    }
+
+    if (email.value === "") {
+        messages.push('Please, enter with your email!')
+        errorElement.style.display = "flex";
+    }
+
+    if (textArea.value === "") {
+        messages.push('What is your message?')
+        errorElement.style.display = "flex";
+    }
+
+    if (messages.length > 0) {
+        e.preventDefault()
+        errorElement.innerText = messages.join(`\n`);
+    }
+
+})
